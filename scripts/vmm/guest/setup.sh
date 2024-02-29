6fe7944d1b9a1a29c2427e7a0872bf8a5e3f6c46
hky1999 <976929993@qq.com>
2024-02-29 15:29:30 +0800

    Add scripts for qemu and jailhouse

---
+++ scripts/vmm/guest/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install packages
+sudo sed -i "s/http:\/\/archive.ubuntu.com/http:\/\/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install -y build-essential python3-mako
+
+# Create a hypervisor image link to /lib/firmware/rvm-intel.bin
+sudo mkdir -p /lib/firmware
+sudo ln -sf ~/arceos-intel.bin /lib/firmware
+
+# Clone jailhouse, apply patches and build
+git clone -b arceos --single-branch https://github.com/arceos-hypervisor/jailhouse-arceos.git
+cd jailhouse-arceos
+make
+
+# Update grub config file to update kernel cmdline
+./update-cmdline.sh
+sudo update-grub
+
+echo
+echo "Setup OK!"
+echo "Press ENTER to reboot..."
+read
+sudo reboot
