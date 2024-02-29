d63af204aff30d896792040d1649a0d1e700d630
hky1999 <976929993@qq.com>
2024-02-29 21:00:16 +0800

    Distinguish linux core, arceos primary core and secondary core

--- modules/axhal/src/platform/x86_linux/mp.rs
+++ modules/axhal/src/platform/x86_linux/mp.rs
@@ -1,5 +1,8 @@
+use core::sync::atomic::Ordering;
+
 /// Starts the given secondary CPU with its boot stack.
 pub fn start_secondary_cpu(_apic_id: usize, _stack_top: crate::mem::PhysAddr) {
    // No need
    // This step is completed by Linux.
+   super::ARCEOS_MAIN_INIT_OK.store(1, Ordering::Release);
 }
