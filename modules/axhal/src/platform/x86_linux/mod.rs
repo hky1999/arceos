4608ff7de3a7b92a6a4e03c41a7fd09cc4a59ee7
hky1999 <976929993@qq.com>
2024-03-01 19:56:32 +0800

    Bug in secondary core boot sequence

--- modules/axhal/src/platform/x86_linux/mod.rs
+++ modules/axhal/src/platform/x86_linux/mod.rs
@@ -33,9 +33,8 @@ use config::HvSystemConfig;
 use header::HvHeader;
 use percpu::PerCpu;
 
-static LINUX_INIT_OK: AtomicU32 = AtomicU32::new(0);
+static PRIMARY_INIT_OK: AtomicU32 = AtomicU32::new(0);
 static ARCEOS_MAIN_INIT_OK: AtomicU32 = AtomicU32::new(0);
-static INIT_LATE_OK: AtomicU32 = AtomicU32::new(0);
 static ERROR_NUM: AtomicI32 = AtomicI32::new(0);
 
 fn has_err() -> bool {
@@ -68,9 +67,11 @@ fn current_cpu_id() -> usize {
     }
 }
 
-fn linux_init_early() {
+fn primary_init_early() {
+    let cpu_id = current_cpu_id();
+
     crate::mem::clear_bss();
-    crate::cpu::init_secondary(current_cpu_id());
+    crate::cpu::init_primary(cpu_id);
     self::uart16550::init();
     // self::dtables::init_primary();
     // self::time::init_early();
@@ -86,62 +87,51 @@ fn linux_init_early() {
         core::str::from_utf8(&system_config.signature),
         system_config.revision,
     );
-    LINUX_INIT_OK.store(1, Ordering::Release);
-}
 
-fn primary_init_early() {
-    let cpu_id = current_cpu_id();
-    axlog::ax_println!("ARCEOS CPU {} primary_init_early()", cpu_id);
-    crate::cpu::init_primary(cpu_id);
+    axlog::ax_println!("ARCEOS CPU {} primary_init_early() ok", cpu_id);
+
+    PRIMARY_INIT_OK.store(1, Ordering::Release);
 }
 
 fn secondary_init_early() {
     let cpu_id = current_cpu_id();
-    axlog::ax_println!("ARCEOS CPU {} secondary_init_early()", cpu_id);
     crate::cpu::init_secondary(cpu_id);
-    // self::dtables::init_secondary();
+
+    axlog::ax_println!("ARCEOS CPU {} secondary_init_early() ok", cpu_id);
 }
 
 extern "sysv64" fn vm_cpu_entry(cpu_data: &mut PerCpu, linux_sp: usize) -> i32 {
-    // Currently we set core 0 as Linux.
-    let is_linux = cpu_data.id == 0;
-    // Currently we set core 1 as main core for arceos.
-    let is_primary = cpu_data.id == 1;
+    let is_primary = cpu_data.id == 0;
+    let online_cpus = HvHeader::get().online_cpus;
 
-    let vm_cpus = HvHeader::get().vm_cpus();
+    wait_for(|| PerCpu::entered_cpus() < online_cpus);
 
-    wait_for(|| PerCpu::entered_cpus() < vm_cpus);
-
-    if is_linux {
-        axlog::ax_println!("{} CPU {} entered.", "Linux", cpu_data.id);
-        linux_init_early();
-
-        axlog::ax_println!("{} CPU {} init ok.", "Linux", cpu_data.id);
-
+    if is_primary {
+        axlog::ax_println!(
+            "{} CPU {} entered.\n header {:#?}",
+            "Linux",
+            cpu_data.id,
+            HvHeader::get()
+        );
+        primary_init_early();
+        unsafe {
+            rust_main(cpu_data.id as usize, 0);
+        }
     } else {
-        wait_for_counter(&LINUX_INIT_OK, 1);
+        wait_for_counter(&ARCEOS_MAIN_INIT_OK, 1);
         axlog::ax_println!(
             "{} CPU {} entered.",
             if is_primary { "Primary" } else { "Secondary" },
             cpu_data.id
         );
-
-        if is_primary {
-            primary_init_early();
-            unsafe {
-                rust_main(cpu_data.id as usize, 0);
-            }
-        } else {
-            wait_for_counter(&ARCEOS_MAIN_INIT_OK, 1);
-            secondary_init_early();
-            unsafe {
-                rust_main_secondary(cpu_data.id as usize);
-            }
+        secondary_init_early();
+        unsafe {
+            rust_main_secondary(cpu_data.id as usize);
         }
     }
 
     let code = 0;
-    axlog::ax_println!(
+    axlog::info!(
         "CPU {} return back to driver with code {}.",
         cpu_data.id,
         code
